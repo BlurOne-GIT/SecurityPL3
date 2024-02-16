a21e46cf8fbb5123a7341df2dfcfbe8ccf774d4d
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-16 20:37:29 -0300

    build: create project

---
+++ src/main/java/code/blurone/securitypl3/SecurityPL3.java
@@ -0,0 +1,17 @@
+package code.blurone.securitypl3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SecurityPL3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
