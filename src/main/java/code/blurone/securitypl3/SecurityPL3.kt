4b1c1c1acac0be4e2477f2317bec533328c0e976
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-16 20:38:42 -0300

    build: kotlin

---
+++ src/main/java/code/blurone/securitypl3/SecurityPL3.kt
@@ -0,0 +1,13 @@
+package code.blurone.securitypl3
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SecurityPL3 : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
