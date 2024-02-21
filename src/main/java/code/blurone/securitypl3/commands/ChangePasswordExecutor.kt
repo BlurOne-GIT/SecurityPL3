4ca68db0fe05fb8002dcd1f7a7d7468829786f38
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-02-21 20:44:33 -0300

    feat: ChangePasswordExecutor

---
+++ src/main/java/code/blurone/securitypl3/commands/ChangePasswordExecutor.kt
@@ -0,0 +1,41 @@
+package code.blurone.securitypl3.commands
+
+import code.blurone.securitypl3.events.PlayerUnauthorizedEvent
+import org.bukkit.Bukkit
+import org.bukkit.NamespacedKey
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabExecutor
+import org.bukkit.entity.Player
+import org.bukkit.persistence.PersistentDataType
+
+class ChangePasswordExecutor(private val authorizedNamespacedKey: NamespacedKey, private val tempPassNamespacedKey: NamespacedKey) : TabExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): Boolean {
+        if (sender !is Player)
+        {
+            sender.sendMessage("You must be a player to use this command.")
+            return true
+        }
+
+        sender.persistentDataContainer.set(authorizedNamespacedKey, PersistentDataType.BOOLEAN, false)
+        sender.persistentDataContainer.set(tempPassNamespacedKey, PersistentDataType.LONG, 0)
+
+        Bukkit.getServer().pluginManager.callEvent(PlayerUnauthorizedEvent(sender))
+
+        return true
+    }
+
+    override fun onTabComplete(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): MutableList<String> {
+        return mutableListOf()
+    }
+}
